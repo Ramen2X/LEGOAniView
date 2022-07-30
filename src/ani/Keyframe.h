24480449ebff35ff12ff48acb2eb22993b28ec90
Ramen2X <64166386+Ramen2X@users.noreply.github.com>
2022-07-30 16:17:06 -0400

    add keyframe header

---
+++ src/ani/Keyframe.h
@@ -0,0 +1,8 @@
+// Information about rotation is currently unknown
+
+struct Keyframe {
+	int ms; // Time the keyframe is positioned in milliseconds
+	float x_pos; // X position of the component
+	float y_pos; // Y position of the component
+	float z_pos; // Z position of the component
+};
