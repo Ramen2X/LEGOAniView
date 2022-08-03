bce485f3d054f3c724b1500531952f5dba29e6aa
Ramen2X <64166386+Ramen2X@users.noreply.github.com>
2022-08-03 17:39:19 -0400

    make keyframe time value unsigned

--- src/ani/Keyframe.h
+++ src/ani/Keyframe.h
@@ -8,7 +8,7 @@ enum Type {
 struct Keyframe {
 	Type type; // Type of the keyframe
 	int num; // Which keyframe number this is
-	short ms; // Time the keyframe is positioned in milliseconds
+	unsigned short ms; // Time the keyframe is positioned in milliseconds
 	float w; // Rotation angle value used in a quaternion
 	float x; // X value of the component
 	float y; // Y value of the component
