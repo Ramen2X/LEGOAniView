cda9298ab92bf350c89b5bdc64d797a286d23570
Ramen2X <64166386+Ramen2X@users.noreply.github.com>
2022-08-03 19:05:10 -0400

    handle actors having no animation data in output

--- src/LEGOAniView.cpp
+++ src/LEGOAniView.cpp
@@ -241,22 +241,25 @@ void LEGOAniView::OutputData()
 
 	for (int i = 0; i < actorsNum; i++) {
 		std::cout << Color::Modifier(Color::FG_CYAN) << "\nAnimation data for actor " << ani.actors[i].name << ":" << Color::Modifier(Color::FG_DEFAULT);
-		for (int i2 = 0; i2 < ani.actors[i].keyframes.size(); i2++) {
-			std::cout << Color::Modifier(Color::FG_GREEN) << "\nKeyframe " << ani.actors[i].keyframes[i2].num << " (" << typeNames[ani.actors[i].keyframes[i2].type] << "):\n" << Color::Modifier(Color::FG_DEFAULT);
+		if (ani.actors[i].keyframes.size() != 0) {
+			for (int i2 = 0; i2 < ani.actors[i].keyframes.size(); i2++) {
+				std::cout << Color::Modifier(Color::FG_GREEN) << "\nKeyframe " << ani.actors[i].keyframes[i2].num << " (" << typeNames[ani.actors[i].keyframes[i2].type] << "):\n" << Color::Modifier(Color::FG_DEFAULT);
 
-			if (useFrames) {
-				std::cout << "Frame: " << Color::Modifier(Color::FG_YELLOW) << (ani.actors[i].keyframes[i2].ms * 0.001) * 60 << "\n" << Color::Modifier(Color::FG_DEFAULT);
-			}
-			else std::cout << "Time: " << Color::Modifier(Color::FG_YELLOW) << ani.actors[i].keyframes[i2].ms << " ms\n" << Color::Modifier(Color::FG_DEFAULT);
+				if (useFrames) {
+					std::cout << "Frame: " << Color::Modifier(Color::FG_YELLOW) << (ani.actors[i].keyframes[i2].ms * 0.001) * 60 << "\n" << Color::Modifier(Color::FG_DEFAULT);
+				}
+				else std::cout << "Time: " << Color::Modifier(Color::FG_YELLOW) << ani.actors[i].keyframes[i2].ms << " ms\n" << Color::Modifier(Color::FG_DEFAULT);
 
-			std::cout << "X: " << Color::Modifier(Color::FG_YELLOW) << std::fixed << std::setprecision(8) << ani.actors[i].keyframes[i2].x << "\n" << Color::Modifier(Color::FG_DEFAULT);
-			std::cout << "Y: " << Color::Modifier(Color::FG_YELLOW) << ani.actors[i].keyframes[i2].y << "\n" << Color::Modifier(Color::FG_DEFAULT);
-			std::cout << "Z: " << Color::Modifier(Color::FG_YELLOW) << ani.actors[i].keyframes[i2].z << "\n" << Color::Modifier(Color::FG_DEFAULT);
-			if (ani.actors[i].keyframes[i2].type == ROTATION && !useEuler) {
-				std::cout << "W (Angle): " << Color::Modifier(Color::FG_YELLOW) << ani.actors[i].keyframes[i2].w << "\n" << Color::Modifier(Color::FG_DEFAULT);
+				std::cout << "X: " << Color::Modifier(Color::FG_YELLOW) << std::fixed << std::setprecision(8) << ani.actors[i].keyframes[i2].x << "\n" << Color::Modifier(Color::FG_DEFAULT);
+				std::cout << "Y: " << Color::Modifier(Color::FG_YELLOW) << ani.actors[i].keyframes[i2].y << "\n" << Color::Modifier(Color::FG_DEFAULT);
+				std::cout << "Z: " << Color::Modifier(Color::FG_YELLOW) << ani.actors[i].keyframes[i2].z << "\n" << Color::Modifier(Color::FG_DEFAULT);
+				if (ani.actors[i].keyframes[i2].type == ROTATION && !useEuler) {
+					std::cout << "W (Angle): " << Color::Modifier(Color::FG_YELLOW) << ani.actors[i].keyframes[i2].w << "\n" << Color::Modifier(Color::FG_DEFAULT);
+				}
+				// To prevent the time/frame count from being ultra-precise
+				std::cout.precision(0);
 			}
-			// To prevent the time/frame count from being ultra-precise
-			std::cout.precision(0);
 		}
+		else std::cout << Color::Modifier(Color::FG_YELLOW) << " None!\n" << Color::Modifier(Color::FG_DEFAULT);
 	}
 }
