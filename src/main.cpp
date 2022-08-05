15329a6cf21d618076351a35f98b77a78d8dc071
Ramen2X <64166386+Ramen2X@users.noreply.github.com>
2022-08-05 16:52:14 -0400

    fix formatting issue

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char* argv[])
 {
 	std::string usageInfo = "\nUsage: LEGOAniView *.ani [options]\n\nOptions:\n"
 		"--use-euler: Prints rotation information in Euler angles as opposed to quaternions\n"
-		"--use - frames: Prints time value of keyframe as a frame value(relative to 60fps) as opposed to milliseconds\n"
+		"--use-frames: Prints time value of keyframe as a frame value(relative to 60fps) as opposed to milliseconds\n"
 		"--help: Shows these usage details\n";
 
 	if (argc < 2) {
